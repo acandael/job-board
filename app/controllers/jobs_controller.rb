32d235f458b4a5e0a3e91023e3a10cf452b4d14f
anthony <info@anthonycandaele.be>
2026-07-01 12:12:09 +0000

    add create jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,4 +6,23 @@ class JobsController < ApplicationController
   def show
     @job = Job.find(params[:id])
   end
+
+  def new
+  @job = Job.new
+  end
+
+  def create
+  @job = Job.new(job_params)
+    if @job.save
+        redirect_to @job, notice: "Your job was posted successfully."
+    else
+        render :new, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def job_params
+  params.expect(job: [ :title, :company, :salary, :region, :apply_url, :apply_email ])
+  end
 end
