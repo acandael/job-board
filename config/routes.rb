d48d94486bbea92bdb71311e190b9703964326bc
anthony <info@anthonycandaele.be>
2026-07-02 15:03:11 +0000

    fix broken jobs route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount MissionControl::Jobs::Engine, at: "/jobs"
+  mount MissionControl::Jobs::Engine, at: "/admin/solid_queue"
   resource :session
   resources :passwords, param: :token
   resource :registration, only: [ :new, :create ]
