358ec4ebeda056e76d5d997bb758ac467d0e9e5e
anthony <info@anthonycandaele.be>
2026-07-02 14:59:51 +0000

    add mission control jobs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount MissionControl::Jobs::Engine, at: "/jobs"
   resource :session
   resources :passwords, param: :token
   resource :registration, only: [ :new, :create ]
